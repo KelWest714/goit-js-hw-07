5a25ab086482527e83ead67ea080838d6b03f2fb
Volodymyr <104516153+KelWest714@users.noreply.github.com>
2022-09-18 14:31:23 +0300

    fix

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(getGalleryMarkup).join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+gallery.addEventListener('click', onGalleryItemOpen);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onGalleryItemOpen(e) {
+  e.preventDefault();
+}
+
+function getGalleryMarkup({ preview, original, description }) {
+  return ` 
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+}
